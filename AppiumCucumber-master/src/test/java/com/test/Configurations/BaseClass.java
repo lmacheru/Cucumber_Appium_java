b20c2401ca240e2e4512f8747b2d5a3bb0864f18
Lehlohonolo Macheru <hlogzaogza@gmail.com>
2021-03-20 15:31:27 +0200

    appium setup

--- AppiumCucumber-master/src/test/java/com/test/Configurations/BaseClass.java
+++ AppiumCucumber-master/src/test/java/com/test/Configurations/BaseClass.java
@@ -7,6 +7,7 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
@@ -18,34 +19,19 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseClass {
     String Config = "src/test/java/com/test/Configurations/Configuration.properties";
     public static ExtentReports extent;
-    static ThreadLocal<AppiumDriverLocalService> service = new ThreadLocal<>();
-    static ThreadLocal<AppiumDriver> driver = new ThreadLocal<>();
-
-    public AppiumDriver getDriver() {
-        return driver.get();
-    }
-    public AppiumDriverLocalService getService() {
-        return service.get();
-    }
+    static AppiumDriver driver ;
 
     /*
     *   Start Appium Server Programmatically before each scenario
     */
     @Before
-    public void startServer() {
-        AppiumServiceBuilder appiumServiceBuilder = new AppiumServiceBuilder()
-                .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
-                .withIPAddress("127.0.0.1");
-        service.set(appiumServiceBuilder.build());
-        service.get().start();
-
-        if (service == null || !service.get().isRunning()) {
-            throw new AppiumServerHasNotBeenStartedLocallyException("An appium server node is not started!");
-        }
+    public void startServer() throws MalformedURLException {
 
         File appDir = new File("src/test/resources/");
         File app = new File(appDir, "FlickrBrowser-cal.zip");
@@ -54,7 +40,10 @@ public class BaseClass {
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
         capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 500000);
         capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-        driver.set(new IOSDriver<IOSElement>(service.get().getUrl(), capabilities));
+
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        driver =new AppiumDriver<MobileElement>(url,capabilities);
+
 
         ExtentSparkReporter spark = new ExtentSparkReporter("Reports/ExtentReportResults.html");
         extent = new ExtentReports();
@@ -66,12 +55,6 @@ public class BaseClass {
     */
     @After
     public void stopServer() {
-        if (driver.get() != null) {
-            driver.get().quit();
-        }
-        if (service.get() != null) {
-            service.get().stop();
-        }
         extent.flush();
     }
 }
