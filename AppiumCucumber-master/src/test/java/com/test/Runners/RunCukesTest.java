ff9f1243aa5ea1e59193faece5c91c406296da99
Lehlohonolo Macheru <hlogzaogza@gmail.com>
2021-03-20 14:28:25 +0200

    Report changes

--- AppiumCucumber-master/src/test/java/com/test/Runners/RunCukesTest.java
+++ AppiumCucumber-master/src/test/java/com/test/Runners/RunCukesTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         monochrome = true,
-        plugin = {"pretty", "html:target/cucumberHtmlReport"},
+        plugin = {"pretty", "html:Reports/ExtentReportResults"},
         features = "src/test/resources/features/",
         glue = {"com.test"}
 )
